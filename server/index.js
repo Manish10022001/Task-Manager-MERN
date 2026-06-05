60976015fbb39c434d04f645f2abcaac60efb01e
Manish10022001 <manishshirsat2850@gmail.com>
2026-06-05 17:01:51 +0530

    Server set up

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI;
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // help to read json from req.body
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
+
+//health endpoint
+app.get("/health", (req, res) => {
+  res.json({ status: "Health OK" });
+});
+
+//database connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  } catch (err) {
+    console.log("Database connection error: ", err);
+    process.exit(1);
+  }
+};
+connectDB();
